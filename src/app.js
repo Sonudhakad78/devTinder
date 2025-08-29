220931933984bb4658c966648526afa49b1354b0
Sonu Dhakad <148325842+Sonudhakad78@users.noreply.github.com>
2025-08-29 19:14:44 +0530

    https methods

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const app = express();
 
 app.get("/user", (req, res) => {
   // logic to save the data
-  res.send({name: "sonu", age: "20", city: "bhopal"});
+  res.send({ name: "sonu", age: "20", city: "bhopal" });
 });
 
 app.get("/user/:name/:Sname", (req, res) => {
   // logic to fetch the data
-  console.log(req.params);   // for printing the request parameters
-    console.log(req.query);    // for printing the query parameters
+  console.log(req.params); // for printing the request parameters
+  console.log(req.query); // for printing the query parameters
   res.send("data is fetched successfully");
 });
 
@@ -21,8 +21,6 @@ app.delete("/user", (req, res) => {
   // logic to delete the data
   res.send("data is deleted successfully");
 });
-
-
 
 //.use is used for all the methods
 app.use("/test", (req, res) => {
