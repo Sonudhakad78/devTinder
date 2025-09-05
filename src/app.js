83246c763cf31cd62363145251a5628c38c4aa64
Sonu Dhakad <148325842+Sonudhakad78@users.noreply.github.com>
2025-09-05 13:15:54 +0530

    db conneted with api

--- src/app.js
+++ src/app.js
@@ -1,40 +1,33 @@
 const express = require("express");
 const app = express();
-const {adminAuth, userAuth} = require("./middlewares/auth");
-app.use("/admin", adminAuth);
-// app.use("/user", userAuth);
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
-app.get("/admin/getData", (req, res)=>{
-  res.send("data is here");
-});
-app.post("/user/login", (req, res)=>{
-  res.send("user logged in successfully");
-})
-app.use("/user", userAuth, (req, res,next)=>{
-  res.send("user is here");
-});
+app.post("/signup", async(req, res) =>{
+  const user = new User({
+    firstName: "Sonu",
+    lastName: "Dhakad",
+    emailId: "dhakadsonu219@gmail.com",
+    age: 20,
+  })
 
-app.delete("admin/deleteData", (req, res)=>{
-  res.send("data deleted"); 
-});
-
-//error handling 
-app.use("/getUserdata", (req, res)=>{
-  //try{
-    // logical errors from the database
-    throw new error ("xyz error");
- // }
-//  catch(err){
-    //res.status(500).send("some error occured");
-  //}
-})
-app.use((err, req, res , next)=>{
-  if(err){
-  res.send("something went wrong");
+  try{
+    await user.save();
+   res.send("user signed up and data saved to db");
   }
+  catch(err){
+    res.status(400).send("error while signing up the user");
+  }
+
 })
 
-
-app.listen(7777, () => {
-  console.log("server is running on port 7777");
-});
+connectDB()
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(7777, () => {
+      console.log("server is running on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("error while connecting to database");
+  });
